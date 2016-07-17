c73c07ef0d9d7ec61306b2af744ab5a8b3054665
Damien Caro <dcaro@microsoft.com>
2016-07-17 09:49:38 +0200

    Looping on files to test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 	execSync = require('child_process').execSync;
 	fs = require('fs');
-	fileName = '2016-05-19-template.md';
+//	fileName = '2016-05-19-template.md';
 	commitRange = process.env['TRAVIS_COMMIT_RANGE'];
 
 console.log(commitRange);
@@ -22,49 +22,51 @@ function GetModifedfiles (commitRange) {
   	return result;
 }
 
-	var ModifiedFiles = GetModifedfiles(commitRange);
-	console.log(ModifiedFiles);
-	console.log(Object.keys(ModifiedFiles).length);
+	var ModifiedFilesArray = GetModifedfiles(commitRange);
+	console.log(ModifiedFilesArray);
+	console.log(Object.keys(ModifiedFilesArray).length);
 
 describe("Check Files modified", function(){
 	it("Nomber of modified files", function () {
-		if (Object.keys(ModifiedFiles).length == 0)	{
+		if (Object.keys(ModifiedFilesArray).length == 0)	{
 			console.log("No files have been modified");			
 		}
 		else {
 			console.log("Several files modified, verifying");
+			for(var filePath in ModifiedFilesArray){
+				var fileName = ModifiedFilesArray[filePath];
+				console.log("Verifiyng file: " + fileName);
+				describe("Check Front Matter", function(){
+					var contents = fs.readFileSync(fileName, 'UTF8');
+					it("Verify presence of tags", function () {
+							
+						//frontmatter = /---\s*([\s\S]*?)\s*---/
+						frontmatter = /---\s*((layout: )+)([ \r\n\S]*?)((title: )+)([ \r\n\S]*?)((author: )+)([ \r\n\S]*?)((date: )+)([ \r\n\S]*?)((color: "blue")+)([ \r\n\S]*?)((excerpt: )+)([ \r\n\S]*?)\s*---/
+						assert.ok (frontmatter.test(contents));
 
-			describe("Check Front Matter", function(){
-				var contents = fs.readFileSync(fileName, 'UTF8');
-				it("Verify presence of tags", function () {
-						
-					//frontmatter = /---\s*([\s\S]*?)\s*---/
-					frontmatter = /---\s*((layout: )+)([ \r\n\S]*?)((title: )+)([ \r\n\S]*?)((author: )+)([ \r\n\S]*?)((date: )+)([ \r\n\S]*?)((color: "blue")+)([ \r\n\S]*?)((excerpt: )+)([ \r\n\S]*?)\s*---/
-					assert.ok (frontmatter.test(contents));
-
-				});
-			});
-			
-			describe("Content of the MD file", function(){
-				var contents = fs.readFileSync(fileName, 'UTF8');
-				it("Verify customer profile", function () {			
-					pattern = /\s*((## [Cc]ustomer [Pp]rofile ##)+)([\s\S]*?)\s*/
-					assert.ok (pattern.test(contents));
+					});
 				});
 				
-				it("Verify conclusion", function () {
-					pattern = /\s*((## [Cc]onclusion ##)+)([\s\S]*?)\s*/
-					assert.ok (pattern.test(contents));
+				describe("Content of the MD file", function(){
+					var contents = fs.readFileSync(fileName, 'UTF8');
+					it("Verify customer profile", function () {			
+						pattern = /\s*((## [Cc]ustomer [Pp]rofile ##)+)([\s\S]*?)\s*/
+						assert.ok (pattern.test(contents));
+					});
+					
+					it("Verify conclusion", function () {
+						pattern = /\s*((## [Cc]onclusion ##)+)([\s\S]*?)\s*/
+						assert.ok (pattern.test(contents));
 
+					});
+
+						it("Verify resources", function () {
+						pattern = /\s*((## [Rr]esources ##)+)([\s\S]*?)\s*/
+						assert.ok (pattern.test(contents));
+
+					});
 				});
-
-					it("Verify resources", function () {
-					pattern = /\s*((## [Rr]esources ##)+)([\s\S]*?)\s*/
-					assert.ok (pattern.test(contents));
-
-				});
-			});
-
+			}
 		}
 
 	});	
